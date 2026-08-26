296299b11c27ae3ec32a1fad6acf8d855775c1a2
IBUNHABIBU <ibunhabib@gmail.com>
2026-08-26 07:31:52 +0300

    Add scaffold

---
+++ spec/factories/customers.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :customer do
+    name { "MyString" }
+    phone { "MyString" }
+    mahali { "MyString" }
+  end
+end
---
+++ spec/views/customers/index.html.tailwindcss_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "customers/index", type: :view do
+  before(:each) do
+    assign(:customers, [
+      Customer.create!(
+        name: "Name",
+        phone: "Phone",
+        mahali: "Mahali"
+      ),
+      Customer.create!(
+        name: "Name",
+        phone: "Phone",
+        mahali: "Mahali"
+      )
+    ])
+  end
+
+  it "renders a list of customers" do
+    render
+    cell_selector = 'div>p'
+    assert_select cell_selector, text: Regexp.new("Name".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("Phone".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("Mahali".to_s), count: 2
+  end
+end
