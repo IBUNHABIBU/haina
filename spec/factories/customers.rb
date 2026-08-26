4c4826c8c8185115439bc9fffb7e241cb9ababc9
IBUNHABIBU <ibunhabib@gmail.com>
2026-08-26 07:54:11 +0300

    Add new scaffold

---
+++ spec/factories/customers.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :customer do
+    name { "MyString" }
+    phone { "MyString" }
+    mahali { "MyString" }
+    user { nil }
+  end
+end
---
+++ spec/views/customers/new.html.tailwindcss_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "customers/new", type: :view do
+  before(:each) do
+    assign(:customer, Customer.new(
+      name: "MyString",
+      phone: "MyString",
+      mahali: "MyString",
+      user: nil
+    ))
+  end
+
+  it "renders new customer form" do
+    render
+
+    assert_select "form[action=?][method=?]", customers_path, "post" do
+
+      assert_select "input[name=?]", "customer[name]"
+
+      assert_select "input[name=?]", "customer[phone]"
+
+      assert_select "input[name=?]", "customer[mahali]"
+
+      assert_select "input[name=?]", "customer[user_id]"
+    end
+  end
+end
